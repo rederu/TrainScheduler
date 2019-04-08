5437aa480f8d3364955c5c11e6aa87941ee5b7f7
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-04-07 19:22:41 -0500

    Added a remove button. Now it deletes the information from the database and also it deletes the selected train's row.

--- assets/js/train.js
+++ assets/js/train.js
@@ -8,9 +8,8 @@ var config = {
   messagingSenderId: "720294003466"
 };
 firebase.initializeApp(config);
-
-
 database = firebase.database();
+
 
 //Initial variables
 var traiName = "";
@@ -19,26 +18,26 @@ var trainTime = 0;
 var trainFreq = 0;
 var minArrival;
 var nexTrain;
+var uptime = 30;
 
-//var valid= moment(trainTime, "HH:mm", true).isValid();
-//$('#train-time').numeric(':');
-var m = moment(trainTime, "HH:mm");
-m.isValid(); // return false
-//Capture button click
-
+//var m = moment(trainTime, "HH:mm");
+//m.isValid(); // return false
 $(document).ready(function () {
-
+  //Capture button click
   $(".add-train").on("click", function (event) {
     event.preventDefault();
-    //Grab values from form boxes
+    //Get values from form boxes
     traiName = $("#train-name").val().trim();
     trainDestination = $("#train-destination").val().trim();
     trainTime = $("#train-time").val().trim();
     trainFreq = $("#train-frequency").val().trim();
+
+    //Avoids empty formulary fields
     if (traiName == "" || trainDestination == "" || trainTime == "" || trainFreq == "") {
       alert("Please fill all the fields");
+      //Avoids invalid First Time times
     } else if (moment(trainTime, "HH:mm").isValid() === false) {
-      alert("Please insert a valid First Train Time (military time)")
+      alert("Please insert a valid First Train Time (military time)");
     } else {
       database.ref().push({
         traiName: traiName,
@@ -52,20 +51,12 @@ $(document).ready(function () {
     }
   }); //End of  click capture
 
+  database.ref().on("child_added", function (snapshot) {
 
-  
-    database.ref().on("child_added", function (snapshot) {
-    //console.log(snapshot.val());
-    //console.log(snapshot.val().traiName);
-    //console.log(snapshot.val().trainDestination);
-    //console.log(snapshot.val().trainTime);
-    //console.log(snapshot.val().trainFreq);
-    //Momentjs
-    //////////////////////////////////////
-    var firsTimeConv = moment(trainTime, "HH:mm").subtract(1, "years");
+    //Time operations and conversion
+    var firsTimeConv = moment(snapshot.val().trainTime, "HH:mm").subtract(1, "years");
     var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
-    //Difference between the times
     var diffTime = moment().diff(moment(firsTimeConv), "minutes");
     //Time apart
     var tRemainder = diffTime % snapshot.val().trainFreq;
@@ -75,24 +66,43 @@ $(document).ready(function () {
     console.log("MINUTES TILL TRAIN: " + minNextArrival);
     var nextArrival = moment().add(minNextArrival, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextArrival).format("HH:mm"));
-    //////////////////////////////////////
+    var key = snapshot.key;
+    console.log(key);
+    //End of momentjs
 
+    //Add values to table
     var tr = $("<tr>");
     var train = "<td>" + snapshot.val().traiName + "</td>";
     var destination = "<td>" + snapshot.val().trainDestination + "</td>";
-    var frequency = "<td>" + snapshot.val().trainFreq + "</td>";
-    var minutes = "<td>" + minNextArrival + "</td>";
-    var timeArrival = "<td>" + moment(nextArrival).format("HH:mm") + "</td>";
-    $(".train-info").append(tr.append(train + destination + frequency + timeArrival + minutes));
+    var frequency = "<td class='text-center timeleft'>" + snapshot.val().trainFreq + "</td>";
+    var minutes = "<td class='text-center nextarrival'>" + minNextArrival + "</td>";
+    var timeArrival = "<td class='text-center timeArrival'>" + moment(nextArrival).format("HH:mm") + "</td>";
+    var delBtn = "<td><button class='btn btn-danger del' key='" + key + "'>X</button></td>";
+    $(".train-info").append(tr.append(train + destination + frequency + timeArrival + minutes + delBtn));
 
 
     // Handle the errors
   }, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
-  });
+  });//end of calling children in database
 
+  timeRightNow();
+}); //end of document ready
 
+function timeRightNow() {
+  var timeNow = moment().format("MMMM DD YYYY hh:mm A");
+  $(".current-time").html(timeNow);
+  setTimeout(timeRightNow, 1000);
+};
+
+//This deletes info from databse
+$(document).on("click", ".del", function () {
+  var keyRef = $(this).attr("key");
+  firebase.database().ref().child(keyRef).remove().key;
+  //window.location.reload();
+  $(this).parents("tr").remove();
 });
+
 
 /*//////////////////////////////
 Card class .train-control
